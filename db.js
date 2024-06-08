e9da42426c358f472806829b78e4a5a02862544d
kwankurn <90328302+heryyy@users.noreply.github.com>
2024-06-09 00:50:06 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config(); // Mengimpor dan mengkonfigurasi dotenv
 
-// Tambahkan log untuk memastikan variabel lingkungan dimuat dengan benar
 console.log('DB_NAME:', process.env.DB_NAME);
 console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PASS:', process.env.DB_PASS);
 console.log('DB_HOST:', process.env.DB_HOST);
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: `/cloudsql/${process.env.DB_HOST}`,
     dialect: 'mysql',
     dialectOptions: {
         socketPath: `/cloudsql/${process.env.DB_HOST}`
